4fefc81ad8885f9a86e919246782b05d0737acb2
fmyzjs <fmyzjs@gmail.com>
2013-08-21 20:04:49 +0800

    解决viewrequest.php  打开空白显示
    解决viewrequest.php  打开空白显示

--- mtpt/viewrequest.php
+++ mtpt/viewrequest.php
@@ -1,7 +1,32 @@
 <?php
 require "include/bittorrent.php";
 dbconn();
-require_once(get_langfile_path());
+$lang_req = array
+(
+	'std_error' => "错误！",
+	'std_invalid_action' => "无效的动作！",
+	'head_req' => "悬赏求种",
+	'name' => "所求资源名称",
+	'ori_amount' => "初始悬赏",
+	'new_amount' => "追加悬赏",
+	'addedby' => "求种人",
+	'addedtime' => "添加时间",
+	'state' => "当前状态",
+	'finished' => "解决",
+	'unfinished' => "未解决",
+	'id_not_exist' => "所求种子不存在",
+	'text_reqed_by' => "由",
+	'text_reqed_at' => "发布于",
+	'ori_amount_is_1' => "根萝卜",
+	'ori_amount_is_2' => "根萝卜",
+	'do' => "行为",
+	'tr_edit' => "编辑",
+	'tr_res' => "我要应求",
+	'id_not_exist' => "所求id不存在",
+	'head_resreq' => "<h2>我要应求</h2>",
+
+);
+
 loggedinorreturn();
 parked();
 $torrentsperpage = 25;
@@ -95,7 +120,7 @@ else {
 		case '6': $column = "comments"; break;
 		case '7': $column = "added"; break;
 		case '8': $column = "finish"; break;
-		case '9': $column = "resetdate"; break;
+		case '9': $column = "resetdate"; break;  //删除这个字段或者req表里面添加这个字段
 		default: $column = "resetdate"; break;
 	}
 	switch($_GET['type']) {
