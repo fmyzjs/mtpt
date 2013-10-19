9e069224aaf4b10aad9ec4fba40e2e9eeed7a9ad
root <root@dev1.os118.bistu.edu.cn>
2013-10-19 20:47:40 +0800

    修改login显示

--- mtpt/login.php
+++ mtpt/login.php
@@ -30,7 +30,7 @@ foreach ($langs as $row)
 $s .= "\n</select>";
 ?>
 
-<p></p>
+<p><?php echo $lang_login['text_QQ']?></p>
 
 <?php
 
@@ -42,78 +42,26 @@ if (!empty($_GET["returnto"])) {
 		print("<p><b>" . $lang_login['p_error']. "</b> " . $lang_login['p_after_logged_in']. "</p>\n");
 	}
 }
-?>
-<form method="post" action="takelogin.php">
-
-<table border="0" cellpadding="5">
-<tr><td class="rowhead">
-<select name="loginmethod">
-<option value="username" selected="selected">用户名</option>
-<option value="email">注册邮箱</option>
-</select >
-</td><td class="rowfollow" align="left"><input type="text" name="username" style="width: 180px; border: 1px solid gray" /></td></tr>
-<tr><td class="rowhead"><?php echo $lang_login['rowhead_password']?></td><td class="rowfollow" align="left"><input type="password" name="password" style="width: 180px; border: 1px solid gray"/></td></tr>
-<?php
-show_image_code ();
-if ($securelogin == "yes") 
-	$sec = "checked=\"checked\" disabled=\"disabled\"";
-elseif ($securelogin == "no")
-	$sec = "disabled=\"disabled\"";
-elseif ($securelogin == "op")
-	$sec = "";
-
-if ($securetracker == "yes") 
-	$sectra = "checked=\"checked\" disabled=\"disabled\"";
-elseif ($securetracker == "no")
-	$sectra = "disabled=\"disabled\"";
-elseif ($securetracker == "op")
-	$sectra = "";
-?>
-
-
-<tr><td class="rowhead">登陆期限</td><td class="rowfollow" align="left">
-<select name="dutime">
-<option value="day" >一天内自动登陆</option>
-<option value="week" selected="selected">一周内自动登陆</option>
-<option value="month">一月内自动登陆</option>
-</select>
-</td></tr>
-
-<tr><td class="toolbox" colspan="2" align="center">
-<input type="submit" value="<?php echo $lang_login['button_login']?>" class="btn" /> <input type="reset" value="<?php echo $lang_login['button_reset']?>" class="btn" />
-<?php
-if ($smtptype != 'none'){
-?>
-</td></tr>
-<tr><td class="toolbox" colspan="2" align="center">
-<p><?php echo $lang_login['p_forget_pass_recover']?> &nbsp&nbsp&nbsp<?php echo $lang_login['p_forget_pass_cardrecover']?> &nbsp  &nbsp <?php echo $lang_login['p_user_log']?></p><a href="confirm_resend.php">重新发送验证邮箱（24h内邮箱验证未通过时使用）</a>
-<p><?php echo $lang_login['text_QQ']?></p><a target="_blank" href="http://wp.qq.com/wpa/qunwpa?idkey=e4a93409574f25aa6e1a29a917ff4b3c97b5db948915d09fbcc073aeb8233bad"><img border="0" src="http://pub.idqqimg.com/wpa/images/group.png" alt="麦田PT新人注册指导" title="麦田PT新人注册指导"></a>
-</td></tr>
-</table>
-<p></p>
-<?php
-
-if (isset($returnto))
-	print("<input type=\"hidden\" name=\"returnto\" value=\"" . htmlspecialchars($returnto) . "\" />\n");
 
 ?>
-</form>
 
 
-
+<table class="main" width="100%">
 
 <?php
-}
 if ($showhelpbox_main != 'no'){?>
-<table width="700" class="main" border="0" cellspacing="0" cellpadding="0"><tr><td class="embedded">
-<h2><?php echo $lang_login['text_helpbox'] ?><font class="small"> -             需要邀请码请到<a href="invitebox.php"target='_blank'><font class="big" color='red'><b>邀请申请区</b></a><font id= "waittime" color="red"></font></h2>
+
+<td width="467" height="500"  align="center" style="border:0;padding-left:10px"  >
+<!-- <h2><?php echo $lang_login['text_helpbox'] ?><font class="small"></font></h2> -->
+
 <?php
-print("<table width='100%' border='1' cellspacing='0' cellpadding='1'><tr><td class=\"text\">\n");
+print("<table width='467'  height='480' valign='top' border='1' cellspacing='0' cellpadding='1'><tr><td class=\"text\">\n");
 	if ($Advertisement->enable_ad()){
 		$shout_ad = $Advertisement->get_ad('shoutlogin');
 		print("<div id=\"ad_shoutindex\">".$shout_ad[0]."</div>");
 	}
 ?>
+
 <script type="text/javascript">
 	var shoutbox_value = 0;
 	setInterval(check_shoutbox_new,2000);
@@ -129,11 +77,73 @@ print("<table width='100%' border='1' cellspacing='0' cellpadding='1'><tr><td cl
         }
 </script>
 <?
-print("<iframe src='" . get_protocol_prefix() . $BASEURL . "/shoutbox.php?type=helpbox' width='650' height='180' frameborder='0' name='sbox' marginwidth='0' marginheight='0'></iframe><br /><br />\n");
-print("<form action='" . get_protocol_prefix() . $BASEURL . "/shoutbox.php' id='helpbox' method='get' target='sbox' name='shbox'>\n");
-print($lang_login['text_message']."<input type='text' id=\"hbtext\" name='shbox_text' autocomplete='off' style='width: 500px; border: 1px solid gray' ><input type='submit' id='hbsubmit' class='btn' name='shout' value=\"".$lang_login['sumbit_shout']."\" /><input type='reset' class='btn' value=".$lang_login['submit_clear']." /> <input type='hidden' name='sent' value='yes'><input type='hidden' name='type' value='helpbox' />\n");
+print("<iframe src='" . get_protocol_prefix() . $BASEURL . "/shoutbox.php?type=helpbox' width='480'  height='350' valign='top' frameborder='0' name='sbox' marginwidth='0' marginheight='0'></iframe><br /><br />\n");
+print("<form action='" . get_protocol_prefix() . $BASEURL . "/shoutbox.php' id='helpbox' width='480' method='get' target='sbox' name='shbox'>");
+print($lang_login['text_message']."<input type='text' id=\"hbtext\" name='shbox_text' autocomplete='off' style='width: 410px; border: 1px solid gray' ><input type='submit' id='hbsubmit' class='btn' name='shout' value=\"".$lang_login['sumbit_shout']."\" /><input type='reset' class='btn' value=".$lang_login['submit_clear']." /> <input type='hidden' name='sent' value='yes'><input type='hidden' name='type' value='helpbox' />\n");
 print("<div id=sbword style=\"display: none\">".$lang_login['sumbit_shout']."</div>");
 print(smile_row("shbox","shbox_text"));
-print("</td></tr></table></form></td></tr></table>");
+print("</td></tr></table></form></td>");
+
 }
+?>
+
+<td  height="500" valign="top" align="center" >
+<!-- <h2><?php echo "登陆" ?><font class="small"> </font></h2> -->
+<!--login-->
+
+<form method="post" action="takelogin.php">
+<div class="login_form" border='0' >
+<div class="login_logo"></div>
+<p><input autocomplete="off" class="text" id="icon_usr" type="text" name="username" placeholder="用户名或注册邮箱"/></p>
+<p><input autocomplete="off" class="text" id="icon_pwd" type="password" name="password" placeholder="密码"/></p>
+<?php show_image_code()?>
+<?php
+
+if ($securelogin == "yes") 
+	$sec = "checked=\"checked\" disabled=\"disabled\"";
+elseif ($securelogin == "no")
+	$sec = "disabled=\"disabled\"";
+elseif ($securelogin == "op")
+	$sec = "";
+
+if ($securetracker == "yes") 
+	$sectra = "checked=\"checked\" disabled=\"disabled\"";
+elseif ($securetracker == "no")
+	$sectra = "disabled=\"disabled\"";
+elseif ($securetracker == "op")
+	$sectra = "";
+?>
+<div class="advance_form">
+<p><input class="checkbox" type="checkbox" name="logout" value="yes" /><?php echo $lang_login['checkbox_auto_logout']?></p>
+<p><input class="checkbox" type="checkbox" name="securelogin" value="yes" /><?php echo $lang_login['checkbox_restrict_ip']?></p>
+<p><input class="checkbox" type="checkbox" name="ssl" value="yes" <?php echo $sec?> /><?php echo $lang_login['checkbox_ssl']?><br /></p>
+<p><input class="checkbox" type="checkbox" name="trackerssl" value="yes" <?php echo $sectra?> /><?php echo $lang_login['checkbox_ssl_tracker']?></p>
+</div>
+
+<p style="height:40px;margin-top:20px;"><span class="links"><a href="signup.php?type=cardreg">校内注册</a> | <a href="recover.php">找回密码</a> | <a href="confirm_resend.php">重发验证邮件</a></span><input type="submit" class="submit" value="登录" class="btn" /></p>
+<?php
+if ($smtptype != 'none'){
+?>
+
+
+
+
+<?php
+
+if (isset($returnto))
+	print("<input type=\"hidden\" name=\"returnto\" value=\"" . htmlspecialchars($returnto) . "\" />\n");
+
+}
+?>
+</div>
+</form>
+<!--login-->
+</td>
+</td>
+</td>
+</table>
+</table>
+
+</div>
+<?php
 stdfoot();
